6f6dff085398612baf2db4565674d18e351311bf
jizou <815371297@qq.com>
2019-04-14 15:44:05 +0800

    http缓存

--- 2019.4.13/server.js
+++ 2019.4.13/server.js
@@ -1,11 +1,34 @@
 const http = require('http')
 const fs = require('fs')
-http.createServer(function(resquest,response){
-    console.log('request come',request.url)
+http.createServer(function (request, response) {
+    console.log('request come', request.url)
 
-    const html = fs.readFileSync('test.html','utf8')
-    response.writeHead(200,{
-        'Content-Type':'text/html'
-    })
-    response.end(html)
+
+    if (request.url === '/') {
+        const html = fs.readFileSync('test.html', 'utf8')
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        })
+        response.end(html)
+    }
+    if(request.url === '/script.js'){
+        const etag = request.headers['if-none-match']
+        if(etag === '777'){
+            response.writeHead(304,{
+                'Content-Type':'text/javascript',
+                'Cache-Control':'max-age=200000,no-cache',
+                'Last-Modified':'123',
+                'Etag':'777'
+            })
+            response.end('')
+        }else{
+            response.writeHead(200,{
+                'Content-Type':'text/javascript',
+                'Cache-Control':'max-age=200000,no-cache',
+                'Last-Modified':'123',
+                'Etag':'777'
+            })
+            response.end('console.log("script loaded")')
+        }
+    }
 }).listen(8888)
